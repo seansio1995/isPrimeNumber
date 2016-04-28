1ee4cf2c67115fe55a3b0f26ad9b1fd488ff1260
seansio1995 <seansio@163.com>
2016-04-28 21:41:54 +0800

    prime determinator

--- isPrime/ViewController.swift
+++ isPrime/ViewController.swift
@@ -10,6 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var enterNum: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    @IBAction func isPrime(sender: AnyObject) {
+        var numberInt=enterNum.text.toInt()
+        if numberInt != nil {
+            var isPrime=true
+            var unwrappedint = numberInt!
+            if unwrappedint == 1
+            {
+                isPrime=false
+            }
+            if unwrappedint == 2
+            {
+                isPrime=true
+            }
+            if unwrappedint > 2
+            {
+                for var i=2; i < numberInt;i++
+                {
+                    if unwrappedint % i == 0
+                    {
+                        isPrime=false
+                    }
+                }
+            }
+            if isPrime == true{
+                answer.text="It is prime"
+            }
+            else
+            {
+                answer.text="It is not prime"
+            }
+            
+        }
+        else
+        {
+            answer.text="Please enter the number"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
